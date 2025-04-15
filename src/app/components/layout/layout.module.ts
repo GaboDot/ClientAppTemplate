701593df289abda8312594979597ee39a763f3d1
Gabriel Flores <floresgabriel1@proveedor.la-bridgestone.com>
2025-04-15 13:18:13 -0600

    Se importa LoadingComponent en plantilla

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { SharedModule } from 'src/app/reusable/shared/shared.module';
+import { LoadingComponent } from './modals/loading/loading.component';
 
 
 
 @NgModule({
   declarations: [
+    LoadingComponent
   ],
   imports: [
     CommonModule,
